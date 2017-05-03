61f00cbe0878a159bc794c299d42be2bb94eae4b
Hermann Vocke <hvocke@thoughtworks.com>
2017-05-03 18:23:03 +0200

    setup go-agent docker container with python and node

--- gocd/agent/agent.sh
+++ gocd/agent/agent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker pull gocd/gocd-agent-alpine-3.5:v17.4.0
-docker run -d -e GO_SERVER_URL=https://172.17.0.2:8154/go gocd/gocd-agent-alpine-3.5:v17.4.0
+docker build -t gocd-agent .
+docker run -d -e GO_SERVER_URL=https://172.17.0.2:8154/go gocd-agent
 
